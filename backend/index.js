67a456c92e4a63948468670d9d59a2bb2b750387
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2025-11-30 16:06:06 +0530

    add forntend url to backend

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://koder-js.vercel.app",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
